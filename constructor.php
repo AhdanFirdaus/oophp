26a1f972b31df9710e4f45cdaed0fb5618320422
AhdanFirdaus <muhammadahdanf1@gmail.com>
2025-04-26 09:45:42 +0700

    feat: add constructor

---
+++ constructor.php
@@ -0,0 +1,23 @@
+<?php 
+
+class Produk {
+  public $judul, $penulis, $penerbit, $harga;
+
+  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0) {
+    $this->judul = $judul;
+    $this->penulis = $penulis;
+    $this->penerbit = $penerbit;
+    $this->harga = $harga;
+  }
+
+  public function getLabel() {
+    return "$this->penulis, $this->penerbit";
+  }
+}
+
+$produk1 = new Produk("naruto", "masashi kishimoto", "shonen jump", 3000);
+$produk2 = new Produk("Free Fire", "epep", "garena", 2500);
+
+echo "Komik : " . $produk1->getLabel();
+echo "<br>";
+echo "Game : " . $produk2->getLabel();
